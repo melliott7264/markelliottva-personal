9acb96b6d01718973d9ef17ea7b67225e4b68884
Mark Elliott <markelliottva@gmail.com>
2022-08-09 16:08:48 -0400

    added gallery plugin and reworked rifle posts

--- wp-content/themes/twentytwentytwo_child/functions.php
+++ wp-content/themes/twentytwentytwo_child/functions.php
@@ -4,11 +4,11 @@
 * Child them to WordPress Twenty Twenty-Two for markelliottva.com
 * Author: Mark Elliott ©2022, all rights reserved
 */
-  
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'twentytwentytwo-child-style', get_stylesheet_uri(),
-        array( 'twentytwentytwo' ), 
-        wp_get_theme()->get('Version') // this only works if you have Version in the style header
-    );
+    wp_enqueue_style( 'twentytwentytwo-child-style', get_stylesheet_uri(), array('twentytwentytwo-style'),wp_get_theme()->get('Version'));
 }
+
+
